6043edb80467c056bbb1511bb427acec0e978764
Manuel Dublanc <manuel.dublanc@gmail.com>
2020-12-09 21:03:48 +0100

    Add a total row and apply some responsive fixes.

--- src/components/App.vue
+++ src/components/App.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="container flex justify-center py-4">
-        <div class="lg:w-1/2 bg-white rounded-lg shadow py-4">
+        <div class="w-full bg-white rounded-lg shadow py-4" style="max-width: 720px">
             <h1 class="text-4xl text-center uppercase text-indigo-600 font-bold border-b border-gray-300 px-4 pb-4 mb-6">
                 Timy
             </h1>
@@ -13,60 +13,73 @@
                 <h2 class="text-2xl text-indigo-600 font-semibold mb-6">Your Time Entries</h2>
                 <!-- TODO: Select day box -->
 
-                <table v-if="entries.length !== 0" class="w-full">
-                    <thead class="text-left">
-                    <tr>
-                        <th class="w-full text-lg border-t border-b-2 border-gray-300 px-4 py-3">Title</th>
-                        <th class="text-lg border-t border-b-2 border-gray-300 px-4 py-3">Start</th>
-                        <th class="text-lg border-t border-b-2 border-gray-300 px-4 py-3">End</th>
-                        <th class="text-lg border-t border-b-2 border-gray-300 px-4 py-3">Duration</th>
-                        <th class="text-lg border-t border-b-2 border-gray-300 px-4 py-3">Action</th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    <template v-for="(entry, index) in entries">
-                        <tr v-if="entry.breakBefore !== false">
-                            <td class="bg-green-800 font-semibold text-lg text-white text-center px-4 py-2" colspan="5">
-                                {{ `Break: ${entry.breakBefore}` }}
+                <div v-if="entries.length !== 0" class="overflow-x-auto">
+                    <table class="w-full">
+                        <thead class="text-left">
+                        <tr>
+                            <th class="w-full text-lg border-t border-b-2 border-gray-300 px-4 py-3">Title</th>
+                            <th class="text-lg border-t border-b-2 border-gray-300 px-4 py-3">Start</th>
+                            <th class="text-lg border-t border-b-2 border-gray-300 px-4 py-3">End</th>
+                            <th class="text-lg border-t border-b-2 border-gray-300 px-4 py-3">Duration</th>
+                            <th class="text-lg border-t border-b-2 border-gray-300 px-4 py-3">Action</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        <template v-for="(entry, index) in entries">
+                            <tr v-if="entry.breakBefore !== false">
+                                <td class="bg-green-800 font-semibold text-lg text-white text-center px-4 py-2"
+                                    colspan="5">
+                                    {{ `Break: ${entry.breakBefore}` }}
+                                </td>
+                            </tr>
+
+                            <tr :class="entry.backgroundColor">
+                                <td class="border-b border-gray-300 px-4 py-3">
+                                    <span v-if="entry.title">{{ entry.title }}</span>
+                                    <span v-else class="text-gray-400">Currently working on ...</span>
+                                </td>
+                                <td class="border-b border-gray-300 px-4 py-3">
+                                    {{ entry.start }}
+                                </td>
+                                <td class="border-b border-gray-300 px-4 py-3">
+                                    <span v-if="entry.end">{{ entry.end }}</span>
+                                    <Spinner v-else/>
+                                </td>
+                                <td class="border-b border-gray-300 text-right whitespace-nowrap px-4 py-3">
+                                    {{ formatDuration(entry.duration) }}
+                                </td>
+                                <td class="border-b border-gray-300 whitespace-nowrap px-4 py-3">
+                                    <button
+                                        class="bg-indigo-600 text-white rounded px-2 py-1 mr-2"
+                                        type="button"
+                                        @click="onEdit(entry)"
+                                    >
+                                        {{ entry.end ? 'Edit' : 'Stop' }}
+                                    </button>
+
+                                    <button
+                                        class="bg-red-600 text-white rounded px-2 py-1"
+                                        type="button"
+                                        @click="onDelete(entry, index)"
+                                    >
+                                        Delete
+                                    </button>
+                                </td>
+                            </tr>
+                        </template>
+
+                        <tr>
+                            <td class="font-semibold text-lg px-4 py-2">
+                                Total:
+                            </td>
+                            <td colspan="2"></td>
+                            <td class="font-semibold text-lg text-right px-4 py-2">
+                                {{ formatDuration(total) }}
                             </td>
                         </tr>
-
-                        <tr :class="entry.backgroundColor">
-                            <td class="border-b border-gray-300 px-4 py-3">
-                                <span v-if="entry.title">{{ entry.title }}</span>
-                                <span v-else class="text-gray-400">Currently working on ...</span>
-                            </td>
-                            <td class="border-b border-gray-300 px-4 py-3">
-                                {{ entry.start }}
-                            </td>
-                            <td class="border-b border-gray-300 px-4 py-3">
-                                <span v-if="entry.end">{{ entry.end }}</span>
-                                <Spinner v-else/>
-                            </td>
-                            <td class="border-b border-gray-300 text-right whitespace-nowrap px-4 py-3">
-                                {{ formatDuration(entry.duration) }}
-                            </td>
-                            <td class="border-b border-gray-300 whitespace-nowrap px-4 py-3">
-                                <button
-                                    class="bg-indigo-600 text-white rounded px-2 py-1 mr-2"
-                                    type="button"
-                                    @click="onEdit(entry)"
-                                >
-                                    {{ entry.end ? 'Edit' : 'Stop' }}
-                                </button>
-
-                                <button
-                                    class="bg-red-600 text-white rounded px-2 py-1"
-                                    type="button"
-                                    @click="onDelete(entry, index)"
-                                >
-                                    Delete
-                                </button>
-                            </td>
-                        </tr>
-                    </template>
-                    </tbody>
-                </table>
+                        </tbody>
+                    </table>
+                </div>
 
                 <div v-else class="text-gray-500">
                     It's time to create your first entry of the day ...
@@ -111,6 +124,10 @@ export default {
         running() {
             return !! this.runningEntry;
         },
+
+        total() {
+            return this.entries.reduce((carry, entry) => carry + entry.duration, 0);
+        },
     },
 
     created() {
@@ -137,11 +154,7 @@ export default {
         },
 
         setRunningEntry(entry) {
-            if (!entry) {
-                this.runningEntry = null;
-            }
-
-            this.runningEntry = { ...entry };
+            this.runningEntry = entry ? { ...entry } : null;
         },
 
         sortEntries() {
--- src/components/PushButtons.vue
+++ src/components/PushButtons.vue
@@ -1,14 +1,14 @@
 <template>
-    <div class="flex -mx-4">
+    <div class="flex flex-wrap -mx-4">
         <template v-if="! running">
-            <div class="w-1/2 px-4 mb-6">
+            <div class="w-full sm:w-1/2 px-4 mb-6">
                 <button
                     class="w-full font-semibold text-2xl text-white uppercase tracking-wide border border-green-900 bg-green-600 hover:bg-green-500 focus:bg-green-500 rounded-xl shadow-lg px-12 py-3"
                     @click="onStart"
                     v-text="'Start'"
                 />
             </div>
-            <div class="w-1/2 px-4 mb-6">
+            <div class="w-full sm:w-1/2 px-4 mb-6">
                 <button
                     class="w-full font-semibold text-2xl text-white uppercase tracking-wide border border-gray-900 bg-gray-600 hover:bg-gray-500 focus:bg-gray-500 rounded-xl shadow-lg px-12 py-3"
                     @click="onAdd"
@@ -18,14 +18,14 @@
         </template>
 
         <template v-else>
-            <div class="w-1/2 px-4 mb-6">
+            <div class="w-full sm:w-1/2 px-4 mb-6">
                 <button
                     class="w-full font-semibold text-2xl text-white uppercase tracking-wide border border-blue-900 bg-blue-600 hover:bg-blue-500 focus:bg-blue-500 rounded-xl shadow-lg px-12 py-3"
                     @click="onSplit"
                     v-text="'Split'"
                 />
             </div>
-            <div class="w-1/2 px-4 mb-6">
+            <div class="w-full sm:w-1/2 px-4 mb-6">
                 <button
                     class="w-full font-semibold text-2xl text-white uppercase tracking-wide border border-red-900 bg-red-600 hover:bg-red-500 focus:bg-red-500 rounded-xl shadow-lg px-12 py-3"
                     @click="onStop"
