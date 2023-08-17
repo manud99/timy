bc9f355999dcd02f1bc9de3e0642475213530582
Manuel Dublanc <19774382+manud99@users.noreply.github.com>
2023-08-17 19:57:10 +0200

    Remove server files.

--- server/routes/timeEntries.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { get } from "../controllers/timeEntries.js";
-import { validateGetRequest } from "../validators/timeEntries.js";
-
-const router = Router();
-
-router.get("/", validateGetRequest, get);
-
-export default router;
--- server/validators/timeEntries.ts
+++
@@ -1,9 +0,0 @@
-import { NextFunction, Request, Response } from "express";
-import type { ValidationError } from "../../@types/ValidationErrors";
-import { validate, validateDate } from "./base.js";
-
-export function validateGetRequest(req: Request, res: Response, next: NextFunction) {
-    validate(req, res, next, (errors) => {
-        validateDate(req.query.date, errors, "date", "Ungültiges Datumsformat");
-    });
-}
